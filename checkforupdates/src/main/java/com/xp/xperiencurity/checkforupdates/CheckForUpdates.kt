d0a5a2f8a0c8f520bb575baa744b5233d723fd37
Kyujiwara <kyujiwara@gmail.com>
2019-02-23 23:39:57 +0800

    First

---
+++ checkforupdates/src/main/java/com/xp/xperiencurity/checkforupdates/CheckForUpdates.kt
@@ -0,0 +1,12 @@
+package com.xp.xperiencurity.checkforupdates
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class CheckForUpdates : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_check_for_updates)
+    }
+}
