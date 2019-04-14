ca3d5562e2e86af303f83d0b8978f2c58e2bdbce
Kyujiwara <kyujiwara@gmail.com>
2019-04-14 18:33:55 +0800

    Combined

--- app/src/main/java/com/xp/xperiencurity/xperiencurity/MainActivity.kt
+++ app/src/main/java/com/xp/xperiencurity/xperiencurity/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.xp.xperiencurity.xperiencurity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.crashlytics.android.Crashlytics
+import io.fabric.sdk.android.Fabric
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Fabric.with(this, Crashlytics())
         setContentView(R.layout.activity_main)
+    }
+
+    fun clickMe(view: View) {
+        startActivity(Intent(this, SupportPage::class.java))
     }
 }
