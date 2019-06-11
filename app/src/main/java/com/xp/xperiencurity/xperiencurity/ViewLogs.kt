42db1a43861702951d33d5bf50286bec5ce7fbf5
Kyujiwara <kyujiwara@gmail.com>
2019-06-11 23:38:27 +0800

    View Logs, manage to download file successfully now

--- app/src/main/java/com/xp/xperiencurity/xperiencurity/ViewLogs.kt
+++ app/src/main/java/com/xp/xperiencurity/xperiencurity/ViewLogs.kt
@@ -91,7 +91,7 @@ class ViewLogs : AppCompatActivity(), CoroutineScope by MainScope() {
                 holder.viewBtn.setOnClickListener {
                     logTitle = holder.logName.text.toString()
                     Toast.makeText(this@ViewLogs, "It is clicked $logTitle", Toast.LENGTH_SHORT).show()
-                    download(logTitle)
+                    download(logTitle.toLowerCase())
                 }
             }
         }
