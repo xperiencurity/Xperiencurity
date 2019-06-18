91bd5dde49472fc5ff8b600d055843cf4754b88f
Demonix <ambicion6629@gmail.com>
2019-06-18 14:41:06 +0800

    more updates on login and register pages

--- app/src/main/java/com/xp/xperiencurity/xperiencurity/RegisterPage.kt
+++ app/src/main/java/com/xp/xperiencurity/xperiencurity/RegisterPage.kt
@@ -1,12 +1,59 @@
 package com.xp.xperiencurity.xperiencurity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_register_page.*
 
 class RegisterPage : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register_page)
+
+
+
+        regbtn.setOnClickListener {
+            setRegister()
+        }
+    }
+
+    fun clickBack(view: View) {
+        startActivity(Intent(this, LoginPage::class.java))
+    }
+
+    private fun setRegister() {
+
+        val email = textEmail.text.toString()
+        val password = textpw.text.toString()
+        val conpassword = textpw2.text.toString()
+
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Please input email/password in the required field", Toast.LENGTH_SHORT).show()
+            return
+        }
+        else if (password.length < 6){
+            Toast.makeText(this, "The minimum characters required for password is six", Toast.LENGTH_SHORT).show()
+        }
+        else if (!password.equals(conpassword)) {
+            Toast.makeText(this, "Both password inputs must be identical", Toast.LENGTH_SHORT).show()
+        }
+        else if (password.equals(conpassword)){
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener {
+                if (!it.isSuccessful) return@addOnCompleteListener
+                val intent = Intent(this, LoginPage::class.java)
+                startActivity(intent)
+                Log.d("Main", "User registered successful with uid: ${it.result?.user?.uid}")
+                Toast.makeText(this, "Register Successful", Toast.LENGTH_SHORT).show()
+            }
+            .addOnFailureListener {
+                Log.d("Main", "Failed to create user: ${it.message}")
+                Toast.makeText(this, "Register Failed, Invalid email address", Toast.LENGTH_SHORT).show()
+            }}
     }
 }
